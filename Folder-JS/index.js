c2ec5f3dab0f319aadcd49bc56292d94432a004d
B.O.A Addington <Franswarduvar>
2022-07-28 16:18:23 -0700

    commiting better file structure and project with inquirer

--- Folder-JS/index.js
+++ Folder-JS/index.js
@@ -1,7 +1,60 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What would you like to call your READ.me file?',
+        name: 'title',
+      },
+      {
+          type: 'input',
+          message: 'How would you describe your project?',
+          name: 'Description',
+        },
+        {
+          type: 'input',
+          message: 'Would you like a section of a table of contents?',
+          name: 'Tcontents',
+        },
+        {
+          type: 'input',
+          message: 'Would you like a section for instalation?',
+          name: 'Installation',
+        },
+        {
+          type: 'input',
+          message: 'Would you like a section for Usage?',
+          name: 'Usage',
+        },
+        {
+            type: 'input',
+            message: 'Would you like a section for Licensing?',
+            name: 'License',
+          },
+          {
+            type: 'input',
+            message: 'Would you like a section for contributions?',
+            name: 'Contributions',
+          },
+          {
+            type: 'input',
+            message: 'Would you like a section for tests?',
+            name: 'Tests',
+          },
+          {
+            type: 'input',
+            message: 'Would you like a section for questions?',
+            name: 'Questions',
+          },
+
+];
+then((response) =>
+console.log(response)
+);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
